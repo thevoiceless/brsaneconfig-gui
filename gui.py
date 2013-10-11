9c13171b8250a7edd680df9c1162a8cc15105d8a
Riley Moses <victi.resurgo@gmail.com>
2013-10-11 14:22:33 -0600

    Get the list of devices and parse the info

--- gui.py
+++ gui.py
@@ -1,15 +1,36 @@
 #! /usr/bin/env python
 
 import sys
+import subprocess
 from PyQt4 import QtGui
 
 
 class ConfigWindow(QtGui.QWidget):
 
+    ID = 0
+    NAME = 1
+    MODEL = 2
+    IP = 3
+
     def __init__(self):
         # The super() method returns the parent object of the given class
         super(ConfigWindow, self).__init__()
+
+        self.printers = []
+        self.gatherInfo()
         self.initUI()
+
+    def gatherInfo(self):
+        output = subprocess.check_output(["brsaneconfig3", "-q"]).splitlines()
+        relevantOutput = output[output.index('Devices on network') + 1:]
+
+        for printerInfo in relevantOutput:
+            id, friendlyName, modelName, ip = printerInfo.split()
+            modelName = modelName.replace('"', '')
+            ip = ip.replace("I:", '')
+            self.printers.append([id, friendlyName, modelName, ip])
+
+        print self.printers
 
     def initUI(self):
         # setGeometry(x, y, width, height) sets window location and size
