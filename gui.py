3f7210dfceb7bbf7cd7ccc488443f04275066c26
Riley Moses <victi.resurgo@gmail.com>
2013-10-11 18:17:44 -0600

    Display dropdown with supported devices, sort list of supported devices

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ from PyQt4 import QtGui
 
 DEBUG = True
 WINDOW_TITLE = 'brsaneconfig3 GUI'
-SCROLLBAR_FUDGE = 20
+SCROLLBAR_FUDGE = 30
 
 class ConfigWindow(QtGui.QMainWindow):
     ID = 0
@@ -47,25 +47,62 @@ class ConfigWindow(QtGui.QMainWindow):
             except:
                 #print "Ignoring model {}, no model name given".format(num)
                 continue
+        self.supportedModels.sort()
         self.deviceList.addItems(self.supportedModels)
 
         # Populate self.myPrinters
         for printerInfo in myPrintersInfo:
-            num, friendlyName, modelName, ip = printerInfo.split()
+            num, friendlyName, modelName, ipOrNode = printerInfo.split()
+            # Check if IP or node name is specified
+            isNode = ipOrNode.startswith("I:")
             # Remove surrounding quotation marks and the 'I:' prefix
-            self.myPrinters.append([num, friendlyName, modelName.replace('"', ''), ip.replace("I:", '')])
+            self.myPrinters.append([num, friendlyName, modelName.replace('"', ''), ipOrNode.replace("I:", ''), isNode])
 
         #self.deviceList.addItems([printer[ConfigWindow.NAME] for printer in self.myPrinters])
 
     def initUI(self):
+        # Device list on left
+        vbox = QtGui.QVBoxLayout()
+        vbox.addWidget(self.deviceList)
+        vbox.addWidget(QtGui.QPushButton("Add New Device"))
+
+        # Main layout
         hbox = QtGui.QHBoxLayout()
-        hbox.addWidget(self.deviceList)
-        hbox.addStretch(1)
+        hbox.addLayout(vbox)
         widgt = QtGui.QWidget()
         widgt.setLayout(hbox)
         self.setCentralWidget(widgt)
+        # Do not allow resizing the devices list
         self.deviceList.setMaximumWidth(self.deviceList.sizeHintForColumn(0) + SCROLLBAR_FUDGE)
+        self.deviceList.setMinimumWidth(self.deviceList.sizeHintForColumn(0) + SCROLLBAR_FUDGE)
 
+        friendlyName = QtGui.QLabel('Name:')
+        friendlyNameEdit = QtGui.QLineEdit()
+
+        modelName = QtGui.QLabel('Model:')
+        modelNameSelect = QtGui.QComboBox()
+        modelNameSelect.addItems(self.supportedModels)
+        # http://stackoverflow.com/a/11254459/1693087
+        # Apply stylesheet to allow limiting max number of items
+        modelNameSelect.setStyleSheet("QComboBox { combobox-popup: 0; }")
+        modelNameSelect.setMaxVisibleItems(10)
+
+        ipLabel = QtGui.QLabel('IP:')
+
+        nodeLabel = QtGui.QLabel('Node:')
+
+        grid = QtGui.QGridLayout()
+        grid.setSpacing(10)
+
+        grid.addWidget(friendlyName, 1, 0)
+        grid.addWidget(friendlyNameEdit, 1, 1)
+
+        grid.addWidget(modelName, 2, 0)
+        grid.addWidget(modelNameSelect, 2, 1)
+
+        hbox.addLayout(grid)
+
+        #hbox.addStretch(1)
         self.resize(400, 250)
         self.setWindowTitle(WINDOW_TITLE)
         self.center()
